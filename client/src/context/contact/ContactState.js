da851cb8a9d1c83f051869a74ed556d8c430131e
stephin007 <stephinreji123@gmail.com>
2020-11-03 00:38:43 +0530

    Added Contact COntext folder ith actions and reducer file

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,70 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+// Creating Initial State
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Stephin reji",
+        email: "mail@stephinreji.me",
+        phone: "8920788383",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Justin reji",
+        email: "mail@justinreji.me",
+        phone: "9968370557",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Sheeja reji",
+        email: "mail@sheejareji.me",
+        phone: "9968750466",
+        type: "personal",
+      },
+    ],
+  };
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //   Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update the contact
+
+  // Filter The contacts
+
+  // CLear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
