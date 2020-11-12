b9f5bb936f071eb64f7a138472678b3731a04556
stephin007 <stephinreji123@gmail.com>
2020-11-13 01:03:17 +0530

    Added filter feature

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,29 +1,29 @@
-import React, {useContext, useRef, useEffect} from "react"
+import React, {useContext, useEffect, useRef} from "react"
 import ContactContext from "../../context/contact/contactContext"
 
-const ContactFilter = () =>{
+const ContactFilter = () => {
     const contactContext = useContext(ContactContext)
     const text = useRef("")
 
     const {filterContacts, clearFilter, filtered} = contactContext
 
     useEffect(() => {
-        if(filtered === null){
+        if (filtered === null) {
             text.current.value = ""
         }
-    }, [])
+    }, [filtered])
 
-    const onChange = e =>{
-        if(text.current.value !== " "){
+    const onChange = e => {
+        if (text.current.value !== "") {
             filterContacts(e.target.value)
-        } else{
+        } else {
             clearFilter()
         }
     }
 
-    return(
+    return (
         <form>
-            <input ref={text} type="text" placeholder="Filter Contacts..." onChange={onchange}/>
+            <input ref={text} type="text" placeholder="Filter Contacts..." onChange={onChange}/>
         </form>
     )
 }
--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,19 +1,28 @@
-import React, { useContext, Fragment } from "react";
+import React, {Fragment, useContext} from "react";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+
 import ContactContext from "../../context/contact/contactContext";
-
-
 import ContactItem from "./ContactItem";
 
 const Contacts = () => {
-  const contactContext = useContext(ContactContext);
-  const { contacts } = contactContext;
-  return (
-    <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
-    </Fragment>
-  );
+    const contactContext = useContext(ContactContext);
+    const {contacts, filtered} = contactContext;
+
+    if (contacts.length === 0) {
+        return <h3 style={{textAlign : "center"}}>Please Add a Contact</h3>
+    }
+
+    return (
+        <Fragment>
+                {filtered !== null
+
+                    ? filtered.map(contact => (
+                    <ContactItem key={contact.id} contact={contact}/>))
+                    : contacts.map(contact => (
+                    <ContactItem key={contact.id} contact={contact}/>))
+                }
+        </Fragment>
+    );
 };
 
 export default Contacts;
