f9f2d582e7dfba15bb3ec31559a6ed733adae40f
stephin007 <stephinreji123@gmail.com>
2020-11-14 01:25:05 +0530

    Added register user method

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import {useReducer} from "react";
+import axios from "axios";
 
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
@@ -27,6 +28,26 @@ const AuthState = (props) => {
     //Load Users
 
     //Register User
+    const register = async formData => {
+        const config = {
+            headers: {
+                "Content-Type" : "application/json" 
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/users', formData, config)
+            dispatch ({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            })
+        } catch (error) {
+            dispatch ({
+                type: REGISTER_FAIL,
+                payload: error.response.data.msg
+            })
+        }
+    }
 
     //Login User
 
