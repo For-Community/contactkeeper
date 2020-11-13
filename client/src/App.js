a1473aab68a76912826c0774beec2ef69c2424ce
stephin007 <stephinreji123@gmail.com>
2020-11-14 01:15:15 +0530

    Added the alerts in the register and the login component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/layouts/Navbar";
+import Alerts from "./components/layouts/Alerts";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/Auth/Register";
@@ -21,12 +22,13 @@ const App = () => {
             <Fragment>
               <Navbar />
               <div className="container">
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/about" component={About} />
-                  <Route exact path="/register" component={Register} />
-                  <Route exact path="/login" component={Login} />
-                </Switch>
+                <Alerts />
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                  </Switch>
               </div>
             </Fragment>
           </Router>
