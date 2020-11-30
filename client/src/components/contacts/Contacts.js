d7778d4925e79914591a767a6291f50810468dbf
stephin007 <stephinreji123@gmail.com>
2020-11-30 21:30:21 +0530

    Added thje context to fetch contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,35 +1,52 @@
-import React, {Fragment, useContext} from "react";
-import {CSSTransition, TransitionGroup} from "react-transition-group";
+import React, { Fragment, useContext, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
+import Spinner from "../layouts/Spinner";
 
 const Contacts = () => {
-    const contactContext = useContext(ContactContext);
-    const {contacts, filtered} = contactContext;
+  const contactContext = useContext(ContactContext);
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-    if (contacts.length === 0) {
-        return <h3 style={{textAlign : "center"}}>Please Add a Contact</h3>
-    }
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []);
 
-    return (
-        <Fragment>
-            <TransitionGroup>
-                {filtered !== null
-                    ? filtered.map(contact => (
-                        <CSSTransition key={contact.id} timeout={500} classNames="item">
-                            <ContactItem contact={contact}/>
-                        </CSSTransition>
-                    ))
-                    : contacts.map(contact => (
-                        <CSSTransition key={contact.id} timeout={500} classNames="item">
-                            <ContactItem contact={contact}/>
-                        </CSSTransition>
-                    ))
-                }
-            </TransitionGroup>
-        </Fragment>
-    );
+  if (contacts !== null && contacts.length === 0 && !loading) {
+    return <h3 style={{ textAlign: "center" }}>Please Add a Contact</h3>;
+  }
+
+  return (
+    <Fragment>
+      {contacts !== null && !loading ? (
+        <TransitionGroup>
+          {filtered !== null
+            ? filtered.map((contact) => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map((contact) => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
+      ) : (
+        <Spinner />
+      )}
+    </Fragment>
+  );
 };
 
 export default Contacts;
